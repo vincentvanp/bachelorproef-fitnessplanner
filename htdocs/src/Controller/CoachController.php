fbf6a7987178a9512a191fab574b5360811fdc9c
vincentvanp <52759139+vincentvanp@users.noreply.github.com>
2023-07-18 18:16:46 +0200

    Small changes in structure

--- htdocs/src/Controller/CoachController.php
+++ htdocs/src/Controller/CoachController.php
@@ -7,16 +7,54 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class CoachController extends AbstractController
 {
-    public function __construct(private readonly EntityManagerInterface $em){}
+    public function __construct(private readonly EntityManagerInterface $em, private readonly MailerInterface $mailer){}
 
     #[Route('/coach', name: 'app_coach')]
     public function index(): Response
     {
         return $this->render('coach/index.html.twig', [
             'clients' => $this->getUser()->getClients(),
+        ]);
+    }
+
+    #[Route(name: 'app_coach_client_add')]
+    public function addClient(Request $request): Response
+    {
+        $defaultData = ['message' => 'Type your message here'];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('email', EmailType::class)
+            ->add('message', TextareaType::class)
+            ->add('send', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+            
+            $email = (new Email())
+                ->from('vincent.vp@icloud.com')
+                ->to($data['email'])
+                ->subject('Time for Symfony Mailer!')
+                ->text($data['message']);
+
+            $this->mailer->send($email);
+
+            return $this->redirectToRoute('app_coach');
+        }
+
+        return $this->render('coach/client-management/add/index.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
