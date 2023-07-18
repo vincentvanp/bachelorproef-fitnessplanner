2ccf7cb9d88c2a2341680e27dc76db85e2fde3b5
vincentvanp <52759139+vincentvanp@users.noreply.github.com>
2023-07-18 14:36:33 +0200

    Linked coaches to clients and made base page

--- htdocs/migrations/Version20230718121451.php
+++ htdocs/migrations/Version20230718121451.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230713154240 extends AbstractMigration
+final class Version20230718121451 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,10 +24,13 @@ final class Version20230713154240 extends AbstractMigration
         $this->addSql('CREATE TABLE training (id INT AUTO_INCREMENT NOT NULL, client_id INT NOT NULL, coach_id INT NOT NULL, title VARCHAR(255) NOT NULL, duration_proposed INT NOT NULL, duration_actual INT NOT NULL, comment_coach VARCHAR(1000) NOT NULL, comment_client VARCHAR(1000) NOT NULL, INDEX IDX_D5128A8F19EB6921 (client_id), INDEX IDX_D5128A8F3C105691 (coach_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, is_trainer TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_role (user_id INT NOT NULL, role_id INT NOT NULL, INDEX IDX_2DE8C6A3A76ED395 (user_id), INDEX IDX_2DE8C6A3D60322AC (role_id), PRIMARY KEY(user_id, role_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE client_coach (client_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_9C34F17B19EB6921 (client_id), INDEX IDX_9C34F17BA76ED395 (user_id), PRIMARY KEY(client_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE training ADD CONSTRAINT FK_D5128A8F19EB6921 FOREIGN KEY (client_id) REFERENCES `user` (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE training ADD CONSTRAINT FK_D5128A8F3C105691 FOREIGN KEY (coach_id) REFERENCES `user` (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_role ADD CONSTRAINT FK_2DE8C6A3A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_role ADD CONSTRAINT FK_2DE8C6A3D60322AC FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE client_coach ADD CONSTRAINT FK_9C34F17B19EB6921 FOREIGN KEY (client_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE client_coach ADD CONSTRAINT FK_9C34F17BA76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
@@ -37,9 +40,12 @@ final class Version20230713154240 extends AbstractMigration
         $this->addSql('ALTER TABLE training DROP FOREIGN KEY FK_D5128A8F3C105691');
         $this->addSql('ALTER TABLE user_role DROP FOREIGN KEY FK_2DE8C6A3A76ED395');
         $this->addSql('ALTER TABLE user_role DROP FOREIGN KEY FK_2DE8C6A3D60322AC');
+        $this->addSql('ALTER TABLE client_coach DROP FOREIGN KEY FK_9C34F17B19EB6921');
+        $this->addSql('ALTER TABLE client_coach DROP FOREIGN KEY FK_9C34F17BA76ED395');
         $this->addSql('DROP TABLE role');
         $this->addSql('DROP TABLE training');
         $this->addSql('DROP TABLE `user`');
         $this->addSql('DROP TABLE user_role');
+        $this->addSql('DROP TABLE client_coach');
     }
 }
---
+++ htdocs/src/Controller/CoachController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CoachController extends AbstractController
+{
+    #[Route('/coach', name: 'app_coach')]
+    public function index(): Response
+    {
+        return $this->render('coach/index.html.twig', [
+            'clients' => $this->getUser()->getClients(),
+        ]);
+    }
+}
