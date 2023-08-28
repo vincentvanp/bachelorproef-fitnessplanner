f63695473664c2a74d7db9729707a37a391266c9
vincentvanp <52759139+vincentvanp@users.noreply.github.com>
2023-08-28 12:40:19 +0200

    Cleaned coach add training

--- htdocs/src/Controller/coach/training/AddController.php
+++ htdocs/src/Controller/coach/training/AddController.php
@@ -5,51 +5,65 @@ namespace App\Controller\coach\training;
 use App\Controller\BaseController;
 use App\Entity\Training;
 use App\Entity\User;
-use App\Form\TrainingType;
+use App\Form\training\coach\TrainingType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AddController extends BaseController
 {
-    #[Route('/coach/training/add/{id}', name: 'app_coach_training_add')]
+    #[Route('/coach/training/add/{withTrainer}/{id}', name: 'app_coach_training_add')]
     public function coachAddTraining(
         EntityManagerInterface $entityManager,
         Request $request,
-        User $client): Response
-    {
+        MailerInterface $mailer,
+        bool $withTrainer,
+        int $id = null,
+    ): Response {
         $training = new Training();
         $coach = $this->getUser();
         $training->setCoach($coach);
-        $training->addClient($client);
+        if (null != $id) {
+            $training->addClient($entityManager->getRepository(User::class)->find($id));
+        }
+        $training->setWithTrainer($withTrainer);
 
         $form = $this->createForm(TrainingType::class, $training);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            /*$email = (new TemplatedEmail()) //TODO Email notifications
-                ->from('vincent.vp@icloud.com')
-                ->to($data['email'])
-                ->subject('Time for Symfony Mailer!')
-                ->htmlTemplate('emails/signup.html.twig')
-                ->context([
-                    'coach' => $this->getUser(),
-                    'adress' => $data['email'],
-                ]);*/
-
-            // $this->mailer->send($email);
-
             $training = $form->getData();
             $training->setDurationActual(0);
-            $training->setWithTrainer(false);
             $entityManager->persist($training);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_coach_training', ['id' => $client->getId()]);
+            foreach ($training->getClients() as $client) {
+                if ($client->getAccountSettings()->isNewTraining()) {
+                    $email = (new TemplatedEmail())
+                        ->from('vincent.vp@icloud.com')
+                        ->to($client->getEmail())
+                        ->subject('New training!')
+                        ->htmlTemplate('emails/notification/new_training.html.twig')
+                        ->context([
+                            'coach' => $this->getUser()->getFullName(),
+                            'client' => $client->getFullName(),
+                        ]);
+
+                    $mailer->send($email);
+                }
+            }
+
+            if (null == $id) {
+                return $this->redirectToRoute('app_coach_personal');
+            }
+
+            return $this->redirectToRoute('app_coach_training', ['id' => $id]);
         }
 
-        return $this->render('coach/client-management/training/add/index.html.twig', [
+        return $this->render('training/coach/add.html.twig', [
             'form' => $form->createView(),
         ]);
     }
---
+++ htdocs/src/Form/training/coach/TrainingType.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Form\training\coach;
+
+use App\Entity\Training;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrainingType extends AbstractType
+{
+    public function __construct()
+    {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Title',
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Give a short description of the training.',
+                ],
+            ])
+            ->add('durationProposed', IntegerType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Proposed duration in minutes',
+                ],
+                'label' => 'Actual duration (minutes)',
+            ])
+            ->add('clients', EntityType::class, [
+                'expanded' => true,
+                'multiple' => true,
+                'class' => User::class,
+                'choices' => $options['data']->getCoach()->getClients(),
+                'choice_label' => function ($choice, string $key, mixed $value): string {
+                    return $choice->getFullName();
+                },
+            ])
+            ->add('startTime', DateTimeType::class, [
+                'widget' => 'single_text',
+                'html5' => true,
+                'data' => new \DateTime('now'),
+            ])
+            ->add('withTrainer', CheckboxType::class, [
+                'label' => 'Is this a personal training session?',
+                'required' => false,
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Add Training',
+                'attr' => ['class' => 'btn btn-info btn-lg btn-block w-100 mt-4 text-white'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Training::class,
+        ]);
+    }
+}
